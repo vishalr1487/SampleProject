260909301d831c0fecf2ffbeb67dc0e1949624cf
vishalr1487 <vishalr1487@gmail.com>
2025-01-09 21:20:03 +0530

    First Commit

---
+++ src/main/java/demoPackage/SampleClassFile.java
@@ -0,0 +1,15 @@
+package demoPackage;
+
+public class SampleClassFile {
+
+	public static void main(String[] args) {
+
+		System.out.println("HELLO WORLD");
+
+		System.out.println("WELCOME TO THE WORLD OF JAVA PROGRAMMING");
+		
+		System.out.println("Bye Bye");
+
+	}
+
+}
